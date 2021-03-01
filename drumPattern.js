3989b9671e0cd6bd81a0e4bac4e7fe7be32e7adf
Hawty1 <49696027+Hawty1@users.noreply.github.com>
2021-03-01 21:36:37 +0100

    add new patterns by clicking the "+" button

--- drumPattern.js
+++ drumPattern.js
@@ -5,9 +5,24 @@ const buttonStatus = (status) => {
     else return status.style.backgroundColor="white";
 };
 
-const addInstrument = (times) => {
-    var newDiv = document.createElement("div");
-    newDiv.id="testingId";
-    newDiv.classList="testingClass";
-    console.log(newDiv)
-}
+const addInstrument = () => {
+
+    // new pattern div
+    var newPatternDiv = document.createElement("div");
+    newPatternDiv.classList="pattern";
+    // new instrument button 
+    var newInstrumentButton = document.createElement("button"); //declare
+    newInstrumentButton.classList="instrument"; // give class name
+    newPatternDiv.appendChild(newInstrumentButton); // append to mother div
+    // new pattern squares
+    for (i=0;i<8;i++){
+        var newPatternSqr = document.createElement("button"); 
+        newPatternSqr.classList="patternSqr";
+        newPatternDiv.appendChild(newPatternSqr);
+    }
+
+    console.log(newPatternDiv)
+//   document.patternTable.appendChild(newPatternDiv)
+   var currentDiv = document.getElementById("patternTable");
+   document.body.insertBefore(newPatternDiv, currentDiv);
+} 
